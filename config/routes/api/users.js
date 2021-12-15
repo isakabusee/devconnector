042baadf43ef237a966e1fd10683b6a4c4db8769
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-12-14 23:48:16 -0800

    fixed routes

--- config/routes/api/users.js
+++ config/routes/api/users.js
@@ -5,6 +5,26 @@ const router = express.Router();
 // @route Get api/users
 // @desc Test route
 // @access Public
-router.get('/', (req, res) => res.send('User route'))
+// router.get('/', (req, res) => res.send('User route'))
+
+router
+    .route("/users")
+    .get((req, res) => {
+        // /users
+        res.send("hi get index/users");
+    })
+    .post((req, res) => {
+        res.send("hi post /index/users");
+    });
+
+router
+    .route("/users/:id")
+    .get((req,res) => {
+        res.send("hi get index/users" + req.params.userid);
+    })
+    .put((req, res) => {
+        res.send("hi put index/users" + req.params.userid);
+    });
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const users = require('./config/routes/api/users');
 
 const app = express();
 
 //Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
+//another method
+app.get("/", (req, res) => {
+    //handle root
+});
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+// module.exports = function(app){
+//     app.get('/api/users', (req, res) => {
+//         res.send('this is the index page!');
+//     })
+// }
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
